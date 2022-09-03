6a8efa870e89aea54f715b57b8093dbd93d85260
SteaceP <steacy@live.ca>
2022-09-03 00:42:56 -0400

    added config ksjd

--- config/database.ts
+++ config/database.ts
@@ -1,14 +1,19 @@
-export default ({ env }) => ({
-  connection: {
-    client: env("DATABASE_CLIENT"),
+const { parse } = require("pg-connection-string");
+
+export default ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password
+      },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};
